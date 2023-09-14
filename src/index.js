0e14836614e398147f1a6549063ef8d2a0025b52
Gunjan Rathore <109368116+Gunjanrathore8178@users.noreply.github.com>
2023-09-14 20:38:34 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter , Routes, Route} from "react-router-dom"
+import {AppProvider} from "./context.js"
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Booklist from "./components/Booklist/BookList"
+import BookDetails from "./components/BookDetails/BookDetails"
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <AppProvider>
+
+  <BrowserRouter>
+    <Routes>
+      
+      <Route path = "/" element={<Home />}>
+
+        <Route path = "about" element={<About />} />
+        <Route path = "book" element={<Booklist />} />
+        <Route path = "/book/:id" element={<BookDetails />} />
+      </Route>
+
+
+    </Routes>
+  </BrowserRouter>
+
+
+  </AppProvider>
+  
+);
